f7db995ad59408cb0206d2e10ec162d1b01a96cb
丘新云 <8127434+qiu-xinyun@user.noreply.gitee.com>
2022-06-03 14:23:09 +0800

    新建项目

---
+++ src/layout/Footer/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="footer flx-center">
+		<a href="http://www.spicyboy.cn/" target="_blank"> 2022 © Geeker-Admin By Geeker Technology. </a>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
